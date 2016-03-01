a613cc4667792e14fc4e63d167bc97992705edb6
Eben <eben>
2016-02-29 21:09:02 -0800

    adds some user interface, with all logical checks for an individual string developed'

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,43 @@
-function arrayMake (userString) {
+function arrMake (userString) {
   return userString.split('');
 };
 
-function vowelSearch (arrayVar) {
-    if (arrayVar[0] == 'a' || 'e' || 'i' || 'o') {
+function vowSearch (arrayVar) {
+    if ((arrayVar[0]) == ('a' || 'e' || 'i' || 'o')) {
       arrayVar.push('ay');
   };
   return arrayVar.join('');
 };
+
+function quSearch (arrayVar) {
+  if (arrayVar[1] == 'q' ) {
+    quSlice = arrayVar.slice(0, 3);
+    for (i = 3; i > 0; i--) {
+      arrayVar.shift(0);
+      alert('popped' + i);
+    };
+    for (i = 0; i < quSlice.length; i++) {
+      a = quSlice[i];
+      alert(quSlice[i]);
+      arrayVar.push(a);
+    };
+    arrayVar.push('ay');
+    return arrayVar.join('');
+  };
+};
+
+function conSearch (arrayVar) {
+  for (i = 0; i <= arrayVar.length; i++) {
+    if ((arrayVar[0]) != ('a' || 'e' || 'i' || 'o')) {
+      arrayVar.push(arrayVar[0]);
+      arrayVar.shift(0);
+    };
+    arrayVar.push('ay');
+  };
+  return arrayVar.join('');
+};
+
+$(function () {
+  console.log(arrMake($('input#stuff').val()));
+  event.preventDefault();
+});
